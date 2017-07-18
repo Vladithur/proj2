17bae8a7a3676d208a454af46bc0850ad91f0498
Vladithur <vladithur@example.com>
2017-07-18 09:18:49 +0300

    commit

--- editor/Editor.java
+++ editor/Editor.java
@@ -1,16 +1,76 @@
 package editor;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.LinkedList;
 
 
 public class Editor extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
+    public LinkedList<Word> words;
+    public int fontSize;
+    public int index_of_word;
+    public int index_in_word;
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Editor");
+        primaryStage.setMinWidth(300);
+        primaryStage.setMinHeight(400);
+        EventHandler<KeyEvent> keyEventHandler =
+                new Editor.KeyEventHandler();
+        Group group = new Group();
+        Scene scene = new Scene(group, 300, 400, Color.WHITE);
+        scene.setOnKeyTyped(keyEventHandler);
+        scene.setOnKeyPressed(keyEventHandler);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private class KeyEventHandler implements EventHandler<KeyEvent> {
+
+        @Override
+        public void handle(KeyEvent keyEvent) {
+            if (keyEvent.getEventType() == KeyEvent.KEY_TYPED) {
+                // Use the KEY_TYPED event rather than KEY_PRESSED for letter keys, because with
+                // the KEY_TYPED event, javafx handles the "Shift" key and associated
+                // capitalization.
+                String characterTyped = keyEvent.getCharacter();
+                if (characterTyped.length() > 0 && characterTyped.charAt(0) != 8) {
+                    // Ignore control keys, which have zero length, as well as the backspace
+                    // key, which is represented as a character of value = 8 on Windows.
+                    Text t = new Text();
+                    keyEvent.consume();
+                }
+
+            } else if (keyEvent.getEventType() == KeyEvent.KEY_PRESSED) {
+                // Arrow keys should be processed using the KEY_PRESSED event, because KEY_PRESSED
+                // events have a code that we can check (KEY_TYPED events don't have an associated
+                // KeyCode).
+                KeyCode code = keyEvent.getCode();
+                if (code == KeyCode.UP) {
+                    fontSize += 5;
+                } else if (code == KeyCode.DOWN) {
+                    fontSize = Math.max(1, fontSize - 5);
+                } else if (code == KeyCode.BACK_SPACE) {
+                    words.get(index_of_word).remove(index_in_word);
+                } else if (code == KeyCode.SPACE) {
+                    words.add(new Word(new Text(0, 0, " ")));
+                    index_of_word++;
+                    index_in_word = 0;
+                }
+            }
+        }
     }
 }
